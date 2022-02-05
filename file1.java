b69c50bc899c998fa5a4d9c3224670e42ccdb61e
Ajay <dhulipallak@outlook.com>
2022-02-05 19:03:30 +0000

    my first commit of source code

---
+++ file1.java
@@ -0,0 +1,17 @@
+total 60
+-rw------- 1 ajay ajay    0 Feb  5 18:18 config-err-NW3j7x
+drwx------ 3 root root 4096 Feb  5 18:16 snap.canonical-livepatch
+drwx------ 3 root root 4096 Feb  5 18:18 snap.snap-store
+drwx------ 2 ajay ajay 4096 Feb  5 18:18 ssh-EafZmFSdpSld
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-colord.service-dDR8kh
+drwx------ 3 root root 4096 Feb  5 18:18 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-fwupd.service-be5zLh
+drwx------ 3 root root 4096 Feb  5 18:18 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-geoclue.service-G6uSYf
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-ModemManager.service-6DhAnf
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-switcheroo-control.service-0wYpsj
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-systemd-logind.service-Avnm7f
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-systemd-resolved.service-xRgevi
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-systemd-timesyncd.service-cOxIdg
+drwx------ 3 root root 4096 Feb  5 18:16 systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-upower.service-ALoNAf
+drwx------ 2 ajay ajay 4096 Feb  5 18:22 Temp-c9a971c8-530e-4b11-9b30-0828e7edc396
+drwx------ 2 ajay ajay 4096 Feb  5 18:22 tracker-extract-files.1000
+drwx------ 2 gdm  gdm  4096 Feb  5 18:16 tracker-extract-files.125
---
+++ file2.java
@@ -0,0 +1,16 @@
+config-err-NW3j7x
+snap.canonical-livepatch
+snap.snap-store
+ssh-EafZmFSdpSld
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-colord.service-dDR8kh
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-fwupd.service-be5zLh
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-geoclue.service-G6uSYf
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-ModemManager.service-6DhAnf
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-switcheroo-control.service-0wYpsj
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-systemd-logind.service-Avnm7f
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-systemd-resolved.service-xRgevi
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-systemd-timesyncd.service-cOxIdg
+systemd-private-c53d0812202e48e89f3a5eb3b0b9a64a-upower.service-ALoNAf
+Temp-c9a971c8-530e-4b11-9b30-0828e7edc396
+tracker-extract-files.1000
+tracker-extract-files.125
